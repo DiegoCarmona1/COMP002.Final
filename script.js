0c40ad168a8c903541ed231d9d40c4ff1a8b63ef
Diego Carmona <diegocarmona@my.whccd.edu>
2023-12-11 20:50:09 -0800

    added foundation const elements and values

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const squares = document.querySelectorAll(".game-square");
+const currentTurn = document.querySelector("#turn");
+const restart = document.querySelector("#button--play-again");
+const xScore = document.querySelector("#scoreboard-x");
+const oScore = document.querySelector("#scoreboard--o");
+const winCond = [
+    [0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
